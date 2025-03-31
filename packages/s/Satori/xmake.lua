e8b1a38d1355c13958ffe2543d5e6e4b621db45c
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-03-31 22:56:16 +0800

    3.2

--- packages/s/Satori/xmake.lua
+++ packages/s/Satori/xmake.lua
@@ -3,10 +3,10 @@ package("Satori")
     set_homepage("https://github.com/yangyangzhong82/Satori-Release")
     set_description("Satori SDK for event recording.")
     add_urls("https://github.com/yangyangzhong82/Satori-Release.git", {alias = "git"})
-    add_urls("https://github.com/yangyangzhong82/Satori-Release/releases/download/$(version)/SatoriSDK.zip", {alias = "zip"})
+    add_urls("https://github.com/yangyangzhong82/Satori-Release/releases/download/$(version)/SDK.zip", {alias = "zip"})
 
     add_versions("git:0.0.1", "f1be82287019ef0321d601500effcaa3d653e814d095286f011d8118c31d66a5")
-    add_versions("zip:0.0.1", "843d12912a016d5dd70403dcbc5bd1d0d07695e6e3d8b1ec604f086b984f1fc5")
+    add_versions("zip:0.0.1", "0c2ff7d48a5b3c8e068afb64a01e3201f8d951049f214811fccae898e689fe70")
 
     add_deps("nlohmann_json")
 
