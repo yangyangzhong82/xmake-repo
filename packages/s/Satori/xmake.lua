d9ff5ab8e085a8a10dc6bd2eb3495663b3a261ac
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-04-02 01:17:50 +0800

    3.83

--- packages/s/Satori/xmake.lua
+++ packages/s/Satori/xmake.lua
@@ -6,13 +6,29 @@ package("Satori")
     add_versions("0.0.1", "899c2a75715244ec8c36d22eb646a4c45a2d288341146e409471ef2862b797ae")
 
     on_install( function (package)
-        os.cp(path.join(package:sourcedir(), "include", "Satori"), package:installdir("include"))
+    local srcdir = package:sourcedir()
+    print("Source directory (package:sourcedir()): ", srcdir)
 
-        -- 2. 复制库文件
-        -- 将解压后的 lib/Satori.lib 文件复制到包安装目录的 lib/ 下
-        -- 目标结构: <install_dir>/lib/Satori.lib
-        os.cp(path.join(package:sourcedir(), "lib", "Satori.lib"), package:installdir("lib"))
-    end)
+    if srcdir then
+        local include_satori_path = path.join(srcdir, "include", "Satori")
+        local lib_satori_path = path.join(srcdir, "lib", "Satori.lib")
+
+        print("Attempting to find source header dir: ", include_satori_path)
+        print("Attempting to find source lib file: ", lib_satori_path)
+
+        -- 检查路径是否存在
+        print("Does include/Satori exist? ", os.isdir(include_satori_path))
+        print("Does lib/Satori.lib exist? ", os.isfile(lib_satori_path))
+
+        -- 暂时注释掉复制操作来调试
+        -- os.cp(include_satori_path, package:installdir("include"))
+        -- os.cp(lib_satori_path, package:installdir("lib"))
+    else
+        print("Error: package:sourcedir() returned nil or empty!")
+    end
+    -- 可以加一句 error() 强制停止安装过程，方便看输出
+    error("Stopping install for debug purposes")
+end)
 
     on_load("windows", function (package)
         -- 添加头文件搜索路径
