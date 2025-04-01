2201b77199ef0bb8f103af9b3ccc721ade05515d
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-04-02 01:12:10 +0800

    3.82

--- packages/s/Satori/xmake.lua
+++ packages/s/Satori/xmake.lua
@@ -5,26 +5,20 @@ package("Satori")
     add_urls("https://github.com/yangyangzhong82/Satori-Release/releases/download/0.0.1/SatoriSDK.zip")
     add_versions("0.0.1", "899c2a75715244ec8c36d22eb646a4c45a2d288341146e409471ef2862b797ae")
 
-    -- on_install 不再需要显式处理路径存储，xmake 默认会解压
-    -- on_install(function (package)
-    -- end)
+    on_install( function (package)
+        os.cp(path.join(package:sourcedir(), "include", "Satori"), package:installdir("include"))
 
-on_load(function (package)
-    -- 直接在安装目录下查找 include 和 lib
-    local includedir = path.join(package:installdir(), "include") -- 移除了 "SatoriSDK"
-    local libdir = path.join(package:installdir(), "lib")       -- 移除了 "SatoriSDK"
+        -- 2. 复制库文件
+        -- 将解压后的 lib/Satori.lib 文件复制到包安装目录的 lib/ 下
+        -- 目标结构: <install_dir>/lib/Satori.lib
+        os.cp(path.join(package:sourcedir(), "lib", "Satori.lib"), package:installdir("lib"))
+    end)
 
-    if os.isdir(includedir) then
-        package:add("includedirs", includedir) -- 添加包含路径
-    else
-        -- 如果需要，可以添加警告或错误信息
-        -- print("warning: include directory not found at " .. includedir)
-    end
-
-    if os.isdir(libdir) then
-        package:add("linkdirs", libdir) -- 添加库文件搜索路径
-    else
-        -- print("warning: lib directory not found at " .. libdir)
-    end
-    package:add("links", "Satori") -- 添加要链接的库名
-end)
+    on_load("windows", function (package)
+        -- 添加头文件搜索路径
+        package:add("includedirs", package:installdir("include"))
+        -- 添加库文件搜索路径
+        package:add("linkdirs", package:installdir("lib"))
+        -- 添加需要链接的库名（xmake 会自动处理 .lib 后缀）
+        package:add("links", "Satori")
+    end)
