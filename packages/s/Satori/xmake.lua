f7dfac1dbb2be5ba1fe2f9f45c315c2a91598a75
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-04-02 11:42:38 +0800

    3.87

--- packages/s/Satori/xmake.lua
+++ packages/s/Satori/xmake.lua
@@ -2,8 +2,8 @@ package("Satori")
     set_homepage("https://github.com/yangyangzhong82/Satori-Release")
     set_description("Satori SDK")
 
-    add_urls("https://github.com/yangyangzhong82/Satori-Release/releases/download/0.0.1/SatoriSDK.zip")
-    add_versions("0.0.1", "6d86b2cfae6a978ef8164aef0c8a4a9cb1369d1dd71edcba25460fa2a94c6a0b")
+    add_urls("https://github.com/yangyangzhong82/Satori-Release/releases/download/$(version)/SatoriSDK.zip")
+    add_versions("0.0.1", "28498798b7f1507fa79b13af85af370ec428c08839fe89863723d25f9df1ca75")
     
 on_install(function(package)
     os.cp("*", package:installdir())
