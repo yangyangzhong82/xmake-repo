6c6b8a840c7dcd3c5ebdd904986fc0c28ecab7d7
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-05-25 00:14:50 +0800

    权限组

--- packages/b/Bedrock-Authority/xmake.lua
+++ packages/b/Bedrock-Authority/xmake.lua
@@ -1,5 +1,5 @@
-package("czmoney")
-    add_urls("https://github.com/yangyangzhong82/czmoney/releases/download/$(version)/czmoney-windows-x64.zip")
+package("Bedrock-Authority")
+    add_urls("https://github.com/yangyangzhong82/Bedrock-Authority/releases/download/$(version)/Bedrock-Authority-windows-x64.zip")
     add_versions("0.0.3", "f1046cf7dbb4fa57cc15c4aaa0ca4da0e72f7372794fd2246f4f477e25e3a414")
     on_install(function (package)
         os.cp("include", package:installdir())
