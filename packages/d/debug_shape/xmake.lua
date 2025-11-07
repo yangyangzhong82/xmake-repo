54ff57b165bd9503fe98508d823a692e91a06dff
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-11-07 10:35:36 +0800

    debugshape

---
+++ packages/d/debug_shape/xmake.lua
@@ -0,0 +1,13 @@
+package("debug_shape")
+    set_license("LGPL-3.0")
+    set_homepage("https://github.com/engsr6982/DebugShape")
+
+    -- <Version, CommitHash>
+    add_urls("https://github.com/engsr6982/DebugShape.git")
+    add_versions("0.1.0", "5b5e740fa130985bc14c615836e2a0dda9a97e7c")
+    add_versions("0.2.0", "7c337b5fd6b0c55c03010f535697655ee3d477b5")
+    add_versions("0.3.0", "6de0232c33223750bf254691350acd7837c47d42")
+    add_versions("0.5.0", "913b935b1f8bb5797168526041b17704771e9ff9f71b33250821ecdd5b28d20e")
+    on_install(function (package)
+        import("package.tools.xmake").install(package)
+    end)
