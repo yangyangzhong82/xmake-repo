1059beae56bffed1229652cf75ada6bff0c945c5
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2026-07-11 22:30:47 +0800

    protocol

--- packages/p/protocol/xmake.lua
+++ packages/p/protocol/xmake.lua
@@ -1,43 +1,43 @@
 package("protocol")
-    set_homepage("https://github.com/yangyangzhong82/Protocol")
-    set_description("Minecraft Bedrock protocol library in C++23")
+    set_homepage("https://github.com/yangyangzhong82/BedrockProtocol")
+    set_description("Minecraft Bedrock v975 protocol library in C++23")
     set_license("MPL-2.0")
 
-    add_urls("https://github.com/yangyangzhong82/Protocol.git")
-    add_versions("dev", "main")
+    add_urls("https://github.com/yangyangzhong82/BedrockProtocol.git")
+    add_versions("v975", "15a144aee446c08cb40823503a2a6d145a120484")
 
-    add_deps("openssl")
-    add_configs("shared", {description = "Build shared library", default = true, type = "boolean"})
+    add_deps("openssl3")
+    add_configs("shared", {description = "Build shared library", default = false, type = "boolean"})
 
     on_install(function (package)
-        if os.isfile("xmake.lua") then
-            import("package.tools.xmake").install(package, {
-                shared = package:config("shared")
-            })
-        else
-            import("package.tools.cmake").install(package, {
-                "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"),
-                "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF")
-            })
+        io.replace("CMakeLists.txt",
+            "add_library(Protocol STATIC ${PROTOCOL_SOURCES})",
+            "add_library(Protocol ${PROTOCOL_SOURCES})",
+            {plain = true})
+
+        local configs = {
+            "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"),
+            "-DREFLECTION_BUILD_TESTS=OFF"
+        }
+        if package:is_plat("windows") then
+            table.insert(configs, "-DCMAKE_CXX_FLAGS=/FIcharconv /FIsystem_error")
+            if package:config("shared") then
+                table.insert(configs, "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON")
+            end
         end
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            #include <vector>
-            #include <cstddef>
-            #include <sculk/protocol/packet/UpdateBlockPacket.hpp>
-            #include <sculk/protocol/utility/deps/BinaryStream.hpp>
+            #include <sculk/protocol/codec/MinecraftPackets.hpp>
+            #include <sculk/protocol/codec/packet/IPacket.hpp>
 
             void test() {
-                std::vector<std::byte> buffer;
-                sculk::protocol::UpdateBlockPacket packet;
-                sculk::protocol::BinaryStream stream(buffer);
-                packet.mRuntimeId = 1;
-                packet.mFlag = 3;
-                packet.mLayer = 0;
-                packet.write(stream);
-                (void)packet.getId();
+                auto packet = sculk::protocol::MinecraftPackets::createPacket(
+                    sculk::protocol::MinecraftPacketIds::Login);
+                (void)packet;
             }
         ]]}, {configs = {languages = "c++23"}}))
     end)
+package_end()
