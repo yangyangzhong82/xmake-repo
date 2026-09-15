ba38c1db03399b6fecf2a07bf071627f9892bbeb
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2026-09-15 14:31:14 +0800

    协议库

--- packages/p/protocol/xmake.lua
+++ packages/p/protocol/xmake.lua
@@ -5,6 +5,7 @@ package("protocol")
 
     add_urls("https://github.com/yangyangzhong82/BedrockProtocol.git")
     add_versions("v975", "15a144aee446c08cb40823503a2a6d145a120484")
+    add_versions("v2168", "49582c04be5412f335ca549a5beb57a1f58f49a1")
 
     add_deps("openssl3")
     add_configs("shared", {description = "Build shared library", default = false, type = "boolean"})
